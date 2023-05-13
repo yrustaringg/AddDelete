e6713021e15f4bdbf029125cd380e69fd5ff1e30
yrustaringg <arwaahmedv@gmail.com>
2023-05-13 11:30:17 +0300

    Initial commit

---
+++ lib/lib/app/modules/home/bindings/home_binding.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
+  }
+}
+
+class Get {
+}
